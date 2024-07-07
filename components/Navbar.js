25351486a8960d99c699f2142708f31fb037e6a1
fortunekenny <gbephraim@gmail.com>
2024-07-07 20:20:25 +0100

    nextjs fundamentals

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,7 @@ const links = [
   { href: "/client", label: "client" },
   { href: "/drinks", label: "drinks" },
   { href: "/tasks", label: "tasks" },
-  { href: "/query", label: "react-query" },
+  { href: "/prisma-example", label: "prisma" },
 ];
 
 const Navbar = () => {
